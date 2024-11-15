f7ef740b6e1cfa78feb93f201589d7821a7aba22
John Hileman <106003582+jhileman07@users.noreply.github.com>
2024-11-15 10:42:44 -0600

    adding directory functionality

---
+++ output/basic/insertion_sort.rs
@@ -0,0 +1,24 @@
+#![allow(
+    dead_code,
+    mutable_transmutes,
+    non_camel_case_types,
+    non_snake_case,
+    non_upper_case_globals,
+    unused_assignments,
+    unused_mut
+)]
+#[no_mangle]
+pub unsafe fn insertion_sort(n: i32, p: *mut i32) {
+    let mut i: i32 = 1 as i32;
+    for i in 1..n {
+        let tmp: i32 = *p.offset(i as isize);
+        let mut j: i32 = i;
+        while j > 0 as i32 && *p.offset((j - 1 as i32) as isize) > tmp {
+            *p.offset(j as isize) = *p.offset((j - 1 as i32) as isize);
+            j -= 1;
+            j;
+        }
+        *p.offset(j as isize) = tmp;
+        i;
+    }
+}
